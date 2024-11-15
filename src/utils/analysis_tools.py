d6c093137f5acd25203d775aafe18fc44423510c
Le0wl <leonie.gasser@epfl.ch>
2024-11-15 15:04:07 +0100

    added documentation to analysis tools

--- src/utils/analysis_tools.py
+++ src/utils/analysis_tools.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import numpy as np
-import polars as pl
 from matplotlib import pyplot as plt
 
-def plot_video_stat(filtered_meta, stat):
-    video_stat = filtered_meta[stat].dropna()
+def plot_video_stat(video_list, stat):
+    """plots histogramms of number based video stats
+
+    Parameters
+    ----------
+    video_list : pd.df
+        the data frame with the videos that are to be plotted
+    stat : str
+        name of the stat that we want to plot. ex: 'view_count'
+    """
+    video_stat = video_list[stat].dropna()
     num_stat = pd.to_numeric(video_stat, errors='coerce')
     if stat =='duration':
         num_stat= num_stat/60/60 #convert from seconds to minutes
@@ -17,8 +25,19 @@ def plot_video_stat(filtered_meta, stat):
     plt.yscale('log')
     plt.show()
 
-def plot_most_common_words(filtered_meta, text, topX):
-    video_text = filtered_meta[text]
+def plot_most_common_words(video_list, text, topX):
+    """plots the most common words in text based video parameters
+
+    Parameters
+    ----------
+    video_list : pd.df
+        the data frame with the videos that are to be plotted
+    text : str
+        name of the text that we want to plot. ex: 'description'
+    topX : int
+        number of words to plot. ex: 10 to get the 10 most common words
+    """
+    video_text = video_list[text]
     video_text= str.split(video_text.to_string(index=False))
     video_text= pd.Series(video_text)
     #filtering
@@ -33,8 +52,17 @@ def plot_most_common_words(filtered_meta, text, topX):
     plt.title('Counts of the '+str(topX)+' most common words in video '+text+' with more than 3 letters')
     plt.show()
 
-def plot_most_common_tags(filtered_meta, topX):
-    video_text = filtered_meta['tags']
+def plot_most_common_tags(video_list, topX):
+    """plots the most common tags of the videos
+
+    Parameters
+    ----------
+    video_list : pd.df
+        the data frame with the videos that are to be plotted
+    topX : int
+        number of tags to plot. ex: 10 to get the 10 most common tags
+    """
+    video_text = video_list['tags']
     video_text= str.split(video_text.to_string(index=False), sep=',')
     video_text= pd.Series(video_text)
     #filtering
@@ -48,8 +76,17 @@ def plot_most_common_tags(filtered_meta, topX):
     plt.title('Counts of the '+str(topX)+' most common words in video tags')
     plt.show()
 
-def plot_text_len_char(filtered_meta, text):
-    video_text = filtered_meta[text]
+def plot_text_len_char(video_list, text):
+    """plots the distribution of text length measured in number of characters
+
+    Parameters
+    ----------
+    video_list : pd.df
+        the data frame with the videos that are to be plotted
+    text : str
+        name of the text that we want to plot. ex: 'title'
+    """
+    video_text = video_list[text]
     text_len= video_text.str.len()
     plt.figure()
     plt.hist(text_len, bins= 100, edgecolor='black')
@@ -60,8 +97,17 @@ def plot_text_len_char(filtered_meta, text):
         plt.yscale('log')
     plt.show()
 
-def plot_text_len_words(meta, text):
-    video_text = meta[text]
+def plot_text_len_words(video_list, text):
+    """plots the distribution of text length measured in number of characters
+
+    Parameters
+    ----------
+    video_list : pd.df
+        the data frame with the videos that are to be plotted
+    text : str
+        name of the text that we want to plot. ex: 'title'
+    """
+    video_text = video_list[text]
     video_text = video_text.astype(str)
     word_count = video_text.apply(lambda x:str.split(x))
     word_count = word_count.apply(lambda x: len(x))
@@ -74,3 +120,4 @@ def plot_text_len_words(meta, text):
     plt.title('Counts of words in Video '+text)
     plt.show()
 
+# def highperformers(video_list, category, percentage):
